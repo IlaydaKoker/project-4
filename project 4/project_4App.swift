5f62e6a1a1883ac857c63dad1b5205415534126c
Ilayda Koker <ilayda.n.koker@gmail.com>
2023-07-13 11:29:20 -0700

    Initial Commit

---
+++ project 4/project_4App.swift
@@ -0,0 +1,17 @@
+//
+//  project_4App.swift
+//  project 4
+//
+//  Created by Scholar on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct project_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
